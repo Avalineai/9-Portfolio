e37f7c252214c1a4cfa00149f4721ae21144730e
Calypso Blue <46878997+Avalineai@users.noreply.github.com>
2019-04-25 23:12:15 -0700

    alt and title added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@ newDivOne.classList.add('project-one')
 let newImgOne = document.createElement('img');
 newImgOne.id = 'projctOne'
 newImgOne.src = "assets/images/project-one.png"
+newImgOne.alt = 'The Rabbit Hole-Project One'
 newDivOne.appendChild(newImgOne)
 let projOne = document.getElementById('one-proj')
 projOne.appendChild(newDivOne)
@@ -15,6 +16,7 @@ newDivTwo.classList.add('project-guess')
 let newImgTwo = document.createElement('img');
 newImgTwo.id = 'wordGuess'
 newImgTwo.src = "assets/images/guardians.png"
+newImgTwo.alt = 'Guardians of the Galaxy Word Guess Game'
 newDivTwo.appendChild(newImgTwo)
 let guessProj = document.getElementById('guess-proj')
 guessProj.appendChild(newDivTwo)
@@ -25,6 +27,7 @@ newDivThree.classList.add('project-gif')
 let newImgThree = document.createElement('img');
 newImgThree.id = 'giftastic'
 newImgThree.src = "assets/images/giftastic.png"
+newImgThree.alt = 'Game of Thrones GifTastic'
 newDivThree.appendChild(newImgThree)
 let gifProj = document.getElementById('gif-proj')
 gifProj.appendChild(newDivThree)
@@ -35,6 +38,7 @@ newDivFour.classList.add('project-trivia')
 let newImgFour = document.createElement('img');
 newImgFour.id = 'giftastic'
 newImgFour.src = "assets/images/triviajpeg.jpg"
+newImgFour.alt = 'Fruits and Vegetable Trivia'
 newDivFour.appendChild(newImgFour)
 let triviaProj = document.getElementById('trivia-proj')
 triviaProj.appendChild(newDivFour)
