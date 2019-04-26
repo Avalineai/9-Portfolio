1a8d1894bbc8fd987c3279474b4d6bd7f4672ce6
Calypso Blue <46878997+Avalineai@users.noreply.github.com>
2019-04-25 22:00:53 -0700

    img hover state added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,9 +5,7 @@ newDivOne.classList.add('project-one')
 var newImgOne = document.createElement('img');
 newImgOne.id = 'projctOne'
 newImgOne.src = "assets/images/project-one.png"
-//append newImgOne to newDivOne
 newDivOne.appendChild(newImgOne)
-//append newDivOne to #proj-box (test)
 var gifProj = document.getElementById('one-proj')
 gifProj.appendChild(newDivOne)
 
@@ -17,9 +15,7 @@ newDivTwo.classList.add('project-guess')
 var newImgTwo = document.createElement('img');
 newImgTwo.id = 'wordGuess'
 newImgTwo.src = "assets/images/guardians.png"
-//append newImgTwo to newDivTwo
 newDivTwo.appendChild(newImgTwo)
-//append newDivTwo to #proj-box (test)
 var gifProj = document.getElementById('guess-proj')
 gifProj.appendChild(newDivTwo)
 
@@ -29,9 +25,7 @@ newDivThree.classList.add('project-gif')
 var newImgThree = document.createElement('img');
 newImgThree.id = 'giftastic'
 newImgThree.src = "assets/images/giftastic.png"
-//append newImgThree to newDivThree
 newDivThree.appendChild(newImgThree)
-//append newDivThree to #proj-box (test)
 var gifProj = document.getElementById('gif-proj')
 gifProj.appendChild(newDivThree)
 
@@ -41,8 +35,6 @@ newDivFour.classList.add('project-trivia')
 var newImgFour = document.createElement('img');
 newImgFour.id = 'giftastic'
 newImgFour.src = "assets/images/triviajpeg.jpg"
-//append newImgFour to newDivFour
 newDivFour.appendChild(newImgFour)
-//append newDivFour to #proj-box (test)
 var gifProj = document.getElementById('trivia-proj')
 gifProj.appendChild(newDivFour)
